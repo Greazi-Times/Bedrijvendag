3f8b1cec6b31025f09f84601506d9e6350d91678
Greazi <mail@greazi.com>
2025-10-23 11:31:12 +0200

    feat: Add middleware alias for editor or admin role in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,10 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+
+        $middleware->alias([
+            'editorOrAdmin' => \App\Http\Middleware\EnsureUserIsEditorOrAdmin::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
