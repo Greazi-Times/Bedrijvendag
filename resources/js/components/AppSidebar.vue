24c999bccf5c311cd82d1417381f4d03bb41c9fc
Greazi <mail@greazi.com>
2025-10-29 11:52:08 +0100

    feat: Update sidebar navigation to include editions and remove company links

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -3,7 +3,7 @@ import NavFooter from '@/components/NavFooter.vue';
 import NavMain from '@/components/NavMain.vue';
 import NavUser from '@/components/NavUser.vue';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
-import { createCompany, dashboard, home } from '@/routes';
+import { createCompany, dashboard, dashEditions, home } from '@/routes';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
 import { Github, LayoutGrid, Book, Users } from 'lucide-vue-next';
@@ -17,23 +17,10 @@ const mainNavItems: NavItem[] = [
     },
 ];
 
-const companyItems: NavItem[] = [
-    {
-        title: 'Add Company',
-        href: createCompany(),
-        icon: LayoutGrid,
-    },
-    {
-        title: 'Edit Company',
-        href: dashboard(),
-        icon: LayoutGrid,
-    },
-];
-
 const editionItems: NavItem[] = [
   {
     title: 'Manage Editions',
-    href: '/dashboard/editions',
+    href: dashEditions(),
     icon: Book,
   },
 ];
@@ -71,7 +58,6 @@ const footerNavItems: NavItem[] = [
 
         <SidebarContent class="space-y-6">
             <NavMain :items="mainNavItems" label="Dashboard" />
-            <NavMain :items="companyItems" label="Companies" />
             <NavMain :items="editionItems" label="Editions" />
             <NavMain :items="userItems" label="Users" />
         </SidebarContent>
